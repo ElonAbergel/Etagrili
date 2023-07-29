3260eb2c3b98f97dfa79e860b9a032cb81e77806
ElonAbergel <elonabergel@gmail.com>
2023-07-29 15:08:08 +0300

    photos

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withImages = require('next-images');
 const withFonts = require('next-fonts');
-const withImages = require('next-images'); // Import next-images plugin
 
 const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
-	output: 'export',
+	// Add the images configuration for image formats
+	// This will support PNG, JPG, JPEG, GIF, and SVG formats
+	images: {
+		domains: [], // Add any domains if you are using external image URLs
+	},
 };
 
-module.exports = withFonts(withImages(nextConfig)); // Add withImages to the chain
+module.exports = withImages(withFonts(nextConfig));
